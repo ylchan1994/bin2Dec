ee04b1f19a8e7e1c084e667f5eef659d24611b3b
Chan YL <ylchan1994@gmail.com>
2024-12-12 07:28:20 +0800

    First Commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
+import { Form, useFetcher } from "react-router";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { constants } from "buffer";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +9,50 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+  const bin = formData.get('bin');
+  const errors: string[] = [];
+  const output = {
+    bin: bin,
+    errors: errors,
+  };
+  
+  if (bin?.toString().match(/[2-9]/)) {
+    output.errors.push('Contains non 0 and 1');
+  } 
+  
+  if (bin?.toString().length > 8) {
+    console.log(bin?.toString().length);
+    output.errors.push('More than 8 character')
+  }
+  return output;
+}
+
+export default function Home({actionData}: Route.ComponentProps) {
+  let fetcher = useFetcher();
+  let bin = fetcher.data?.bin;
+  let errors = fetcher.data?.errors; 
+  
+  //console.log(bin, errors)
+  errors = errors && errors.length === 0? null : errors;
+  
+  return (
+    <>
+      <fetcher.Form method="post">
+        <div>
+          <p>Binary Number</p>
+          <input type="number" name="bin" id="bin"></input>
+          <button type="submit">Convert</button>
+        </div>
+        <div>
+          {errors? (
+            errors.map((error: String, index: number) => (
+              <p key={index}>{error}</p>
+            ))
+          ): parseInt(bin?.toString(),2)}
+        </div>
+      </fetcher.Form>
+    </>
+  );
 }
